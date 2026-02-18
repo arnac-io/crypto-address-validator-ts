4097fb25bd26d144d36f7284bbc36977aa80032f
Ivan Fridman <ivan@fordefi.com>
2026-02-18 12:24:59 +0200

    Add pearl support for address validator

---
+++ src/validators/pearl_validator.ts
@@ -0,0 +1,7 @@
+import { Currency, Options } from "../types/types";
+
+var segwit = require('../crypto/externals/segwit_addr');
+
+export function isValidAddress(address: string, currency: Currency, opts: Options): boolean {
+    return segwit.isValidAddress(address, currency, opts);
+}
